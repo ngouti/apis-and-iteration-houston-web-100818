685a136aa8b2eaaa66faa1903454fa6454f3a862
ngouti <gouti.naama@gmail.com>
2018-10-09 17:41:32 -0500

    ng

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,6 +2,8 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
+
+
 def get_character_movies_from_api(character)
   #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
@@ -9,8 +11,24 @@ def get_character_movies_from_api(character)
   
   # NOTE: in this demonstration we name many of the variables _hash or _array. 
   # This is done for educational purposes. This is not typically done in code.
+  
+  # for(x = 0; x <= response_hash['results'].length-1; x++)
+  # response_hash['results'].each do |character_data|
+  #   character_data.each do |key, val|
+  #     if key == "name"
+  #       if character_data[key] == character
+  #         binding.pry
+  y = []
+  x = response_hash['results'].find {|data| data["name"].downcase == character}
+  films = x['films']
+  array = films.collect do |url|
+    resp_string = RestClient.get(url)
+      resp_hash = JSON.parse(resp_string)
+  end
 
+  
 
+  
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
@@ -20,10 +38,15 @@ def get_character_movies_from_api(character)
   # this collection will be the argument given to `parse_character_movies`
   #  and that method will do some nice presentation stuff: puts out a list
   #  of movies by title. play around with puts out other info about a given film.
+
 end
+
 
 def print_movies(films_hash)
   # some iteration magic and puts out the movies in a nice list
+  films_hash.collect do |hash|
+    puts hash["title"]
+  end
 end
 
 def show_character_movies(character)
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,10 @@
 def welcome
   # puts out a welcome message here!
+  puts "Luke, I am your Star Wars movie info finder in a galaxy far, far away."
 end
 
 def get_character_from_user
-  puts "please enter a character"
+  puts "Please enter a character"
   # use gets to capture the user's input. This method should return that input, downcased.
+ character = gets.chomp.downcase
 end
